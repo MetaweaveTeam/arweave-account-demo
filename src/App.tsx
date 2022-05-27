38b79e24dabbced1600ac07246f97a7273ad24ee
bidetaggle <bidetaggle@gmail.com>
2022-05-27 14:20:06 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import useArConnect from 'use-arconnect';
+import CancelIcon from '@mui/icons-material/Cancel';
+import Profile from './Profile';
+
+const arConnectPermissions = [
+  "ACCESS_ADDRESS",
+  "ACCESS_ALL_ADDRESSES",
+  "SIGN_TRANSACTION",
+];
 
 function App() {
+  const arConnect = useArConnect();
+  const [walletAddr, setWalletAddr] = useState<string>("");
+
+  useEffect(() => {
+    if (!arConnect) return;
+    (async () => {
+      try {
+        if ((await arConnect.getPermissions()).includes("ACCESS_ADDRESS")) {
+          setWalletAddr(await arConnect.getActiveAddress());
+        }
+      } catch {
+        alert("Error: Could not get ACCESS_ADDRESS permission");
+      }
+    })();
+  }, [arConnect, walletAddr, setWalletAddr]);
+
+  const connectWallet = async () => {
+    if (!arConnect) return window.open("https://arconnect.io");
+    try {
+      await arConnect.connect(arConnectPermissions);
+      setWalletAddr(await arConnect.getActiveAddress());
+    } catch {
+      alert("Error: Could not connect to ArConnect");
+    }
+  };
+
+  const disconnectWallet = async () => {
+    await arConnect.disconnect();
+    setWalletAddr("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {walletAddr === ""
+      ? <div onClick={connectWallet} style={{cursor: "pointer"}}>
+          <img src="https://7raizsvioke4bdkyrwmaucyzjec2cbzx5mnavefm33yidjezezvq.arweave.net/_ECMyqhyicCNWI2YCgsZSQWhBzfrGgqQrN7wgaSZJms" alt="ArConnect" />
+          <span className="text">{arConnect ? 'Log in' : 'Sign up'}</span>
+        </div>
+      : <>
+          <div onClick={disconnectWallet} style={{cursor: "pointer"}}>
+            <CancelIcon /><span className="text">Logout</span>
+          </div>
+          <Profile walletAddr={walletAddr} />
+        </>}
     </div>
   );
 }
---
+++ src/Profile.tsx
@@ -0,0 +1,38 @@
+import Account from 'arweave-account';
+import { T_profile } from 'arweave-account/lib/types';
+import { useEffect, useState } from 'react';
+
+function Profile({walletAddr}: {walletAddr: string}) {
+  const [userProfile, setUserProfile] = useState<T_profile | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const account = new Account();
+      const a = await account.find("Mitchell13#WL65Eg");
+      if(a?.profile) setUserProfile(a.profile);
+    })();
+  }, [walletAddr]);
+  
+  return(
+    <>
+      <div>
+        {userProfile?.avatar
+          ? <img src={`https://arweave.net/${userProfile.avatar}`} width={200} alt="avatar" />
+          : <img src={`https://brhqy4254yjhasbiflsgacrvnz6xicjhybcst7nle4vxlrjjpe.arweave.net/DE8Mc13mEnBIKCrkY-Ao1bn10CSfARSn9qycrdcUpeU`} width={200} alt="default avatar" />
+        }
+      </div>
+      <div>
+        {userProfile?.handle
+          ? userProfile.handle
+          : walletAddr
+        }
+      </div>
+      <hr />
+      <pre style={{textAlign: 'left'}}>
+        {JSON.stringify(userProfile, null, 4)}
+      </pre>
+    </>
+  );
+}
+
+export default Profile;
